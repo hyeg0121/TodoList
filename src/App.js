424e10848e26dbdff8c9cdf5de073114efe35273
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-24 14:57:25 +0900

    feat: Cat API 요청

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Spinner } from "react-bootstrap";
+import CardContainer from "./components/CardContainer";
+
 function App() {
+
+  const [breeds, setBreeds] = useState([])
+  const [page, setPage] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://api.thecatapi.com/v1/breeds?limit=10&page=${page}`, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.REACT_APP_X_API_KEY
+        }
+      });
+      console.log(response.data);
+      setBreeds(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.error('Error fetching the cat image:', error);
+    }
+  };
+
   return (
-    <div>Hello</div>
+    <Container >
+      {
+        loading ?
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+          :
+          <CardContainer dataList={breeds} />
+      }
+    </Container>
   );
 }
 
