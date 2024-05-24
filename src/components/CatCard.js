4ab97d91f495b88f728941340602427f98341083
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-24 14:58:03 +0900

    feat: CardItem 추가

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -1,6 +1,25 @@
-function CatCard({ data }) {
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+
+
+function CatCard({ idx, imgSrc, name, description, origin }) {
+
+
     return (
-        <div>{data}</div>
+        <Col key={idx}>
+            <Card className='h-100'>
+                <Card.Img variant="top" src={imgSrc} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small>{origin}</small>
+                </Card.Footer>
+            </Card>
+        </Col>
     )
 }
 
