65320cd0a5b40d2029ff60690d47512bcd77d22f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-08 20:25:23 +0900

    Feat: loading 상태 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import { Container} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 import TodoAdder from "./components/TodoAdder";
-function App() {
+import TodoList from "./components/TodoList";
 
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchTodoList();
+  }, [loading]);
+
+  const fetchTodoList = () => {
+    fetch(`http://localhost:8080/api/todos`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        setTodoList(data);
+        setLoading(false);  // 데이터 가져오기 완료 후 loading을 false로 설정
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setLoading(false);  // 에러 발생 시에도 loading을 false로 설정
+      });
+  };
 
   return (
     <Container className="my-4">
       <h1>TODO APP</h1>
-      <TodoAdder />
+      <TodoAdder setLoading={setLoading} />
+      <TodoList todoList={todoList} setLoading={setLoading} />
     </Container>
   );
 }
