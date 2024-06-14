c7cafc36e278e70f101643bd7a36f6e7c2dfe793
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-14 14:17:29 +0900

    refactor: setLoading 제거

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,37 +1,38 @@
+import React from 'react';
 import Table from 'react-bootstrap/Table';
 import TodoItem from './TodoItem';
 
-function TodoList({ todoList, setLoading }) {
+function TodoList({ todoList, handleDeleteTodo, handleEditTodo }) {
     return (
-        <Table hover className="mt-5">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>카테고리</th>
-                    <th>할 일</th>
-                    <th>마감일</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    todoList.map((todo, idx) =>
+        <div className="mt-5">
+            {todoList.length === 0 ? (
+                <p>데이터가 없습니다.</p>
+            ) : (
+                <Table hover>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>카테고리</th>
+                        <th>할 일</th>
+                        <th>마감일</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {todoList.map((todo, idx) => (
                         <TodoItem
                             key={todo.id}
-                            id={todo.id}
-                            idx={idx + 1}
-                            task={todo.task}
-                            ctgColor={todo.ctgColor}
-                            korName={todo.korName}
-                            deadline={todo.deadline}
-                            completed={todo.completed}
-                            setLoading={setLoading}
+                            index={idx + 1}
+                            todo={todo}
+                            handleDeleteTodo={handleDeleteTodo}
+                            handleEditTodo={handleEditTodo} // Pass handleEditTodo function
                         />
-                    )
-                }
-            </tbody>
-        </Table>
-    )
+                    ))}
+                    </tbody>
+                </Table>
+            )}
+        </div>
+    );
 }
 
 export default TodoList;
